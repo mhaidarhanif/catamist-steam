fdbb7e2fc577b9aa966342328ac5c1233b02c7e5
M Haidar Hanif <git@mhaidarhanif.com>
2022-08-04 23:41:07 +0700

    feat: LinkButton props className

--- src/components/link-button.tsx
+++ src/components/link-button.tsx
@@ -1,12 +1,16 @@
+import { clsx } from "clsx";
+
 export function LinkButton({
   to,
+  className,
   children,
 }: {
   to: string;
+  className: string;
   children: React.ReactNode;
 }) {
   return (
-    <a className="button" href={to}>
+    <a className={clsx("button", className)} href={to}>
       {children}
     </a>
   );
