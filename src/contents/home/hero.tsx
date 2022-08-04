fcea40bf65ed206df2ab732e97a8c7e83ed64e8e
M Haidar Hanif <git@mhaidarhanif.com>
2022-08-04 23:08:31 +0700

    feat: home hero with background image

--- src/contents/home/hero.tsx
+++ src/contents/home/hero.tsx
@@ -1,3 +1,20 @@
+import { clsx } from "clsx";
+
 export function HomeHero() {
-  return <div>Home Hero</div>;
+  return (
+    <div
+      className={clsx(
+        "bg-home-hero",
+        "flex h-[420px] items-center justify-center bg-slate-900"
+      )}
+    >
+      <div className="text-center">
+        <h1 className="text-8xl leading-tight">
+          Catamyst
+          <br />
+          Festival
+        </h1>
+      </div>
+    </div>
+  );
 }
