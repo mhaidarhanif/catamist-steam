106695e000a683bad14b9c20ec0ae98c1007b6b2
M Haidar Hanif <git@mhaidarhanif.com>
2022-08-04 21:39:01 +0700

    refactor: rename apps to app for main

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 
 import "./index.css";
 
-import App from "./apps";
+import App from "./app";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
